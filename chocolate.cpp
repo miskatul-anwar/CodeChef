09e93e5c2fa203f74b2d75f33531c102bf64237a
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-29 09:24:08 +0600

    Fri Mar 29 09:24:08 AM +06 2024

---
+++ chocolate.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // your code goes here
+  int t, x, y, z;
+  cin >> t;
+  while (t--) {
+    cin >> x >> y >> z;
+    cout << (5 * x + 10 * y) / z << endl;
+  }
+  return 0;
+}
