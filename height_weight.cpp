27a242fe92dde4f27383439a719dae36639f8ff6
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-29 18:03:55 +0600

    Fri Mar 29 06:03:55 PM +06 2024

---
+++ height_weight.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // your code goes here
+  int height, weight;
+  cin >> weight >> height;
+  height <= 130 && weight >= 60 ? cout << "YES" << endl : cout << "NO" << endl;
+  return 0;
+}
+
