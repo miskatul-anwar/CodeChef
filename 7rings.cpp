657e66b7ac2373a25212b50d56e345d29daf9b2c
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-04-01 09:29:23 +0600

    Mon Apr  1 09:29:23 AM +06 2024

---
+++ 7rings.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // your code goes here
+  int a, b, t, x, count;
+  cin >> t;
+  while (t--) {
+    count = 0;
+    cin >> a >> b;
+    x = a * b;
+    while (x != 0) {
+      x /= 10;
+      count++;
+    }
+    count == 5 ? cout << "Yes" << endl : cout << "No" << endl;
+  }
+  return 0;
+}
